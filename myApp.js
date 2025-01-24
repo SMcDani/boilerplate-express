7a9e773f11194f044468888f610bdb8cfcf821c1
SMcDani <Sbird7134@gmail.com>
2025-01-23 21:00:50 -0500

    FCC Challenges 5-8
    use .env file, implement a logger, chain middleware, ger param input from client

--- myApp.js
+++ myApp.js
@@ -1,13 +1,36 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 console.log("Hello World");
+
+app.use(function(req, res, next){
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 
 app.get('/', function(req, res){res.sendFile(__dirname + '/views/index.html')});
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/json', function(req, res){res.json({"message": "Hello json"})})
+/*app.get('/json', function(req, res){
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+        res.json({"message": "HELLO JSON"});
+    } else{
+        res.json({"message": "Hello json"});
+    }
+    
+})*/
+
+/*app.get('/now', function(req, res, next){
+    req.time = new Date().toString();
+    next();
+    }, function(req, res){
+        res.send({time: req.time});
+    });*/
+
+//#8
+app.get('/:word/echo', (req, res) => {res.send({'echo': req.params.word})});
 
 
 
